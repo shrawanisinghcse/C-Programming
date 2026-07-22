25511c7ce7c6774a64ae23050085fd674cf06cdf
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-07-22 21:46:49 +0530

    Number sign finder

---
+++ Basics/Positive_Negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+
+    int n;
+    printf("Enter Number : ");
+    scanf("%d", &n);
+
+    if(n > 0) {
+        printf("POSITIVE");
+    }
+
+    else if(n < 0) {
+        printf("NEGATIVE");
+    }
+
+    else {
+        printf("ZERO");
+    }
+
+    return 0;
+}
