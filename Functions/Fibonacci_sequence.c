40b3a2887a1421bd1cd5ab273ece51d15aa15b49
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-07-24 14:13:27 +0530

    Fibonacci Sequence

---
+++ Functions/Fibonacci_sequence.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int fib(int n);
+
+int main(){
+
+    int n;
+    printf("Enter Number : ");
+    scanf("%d", &n);
+
+    printf("%d",fib(n));
+
+    return 0;
+}
+
+int fib(int n) {
+    if(n == 0 || n==1)  {
+        if(n==0) {
+        return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+    }
+    int fibNm1 = fib(n-1);
+    int fibNm2 = fib(n-2);
+    int fibN = fibNm1 + fibNm2;
+    return fibN; 
+}
