090a4dd6bf32a20e436dbd55cdfd6294adde1197
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-08-19 19:17:49 +0530

    Printing numbers in reverse order

---
+++ Arrays/Reverse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+   
+    int a[] = {34,56,54,32,67,89,90,32,21};
+
+    
+    for(int i = 8;i >= 0; i--) {
+        printf("%d \n", a[i]);
+    }
+    return 0;
+}
