08a0a2cedb17265e1af23f824cfc00976ab86593
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-08-26 12:25:44 +0530

    Table of 2 and 3 using 2D array

--- Arrays/Table2DArray.c
+++ Arrays/Table2DArray.c
@@ -4,5 +4,17 @@ void storeTable(int arr[] [10], int n, int m, int number );
 
 int main() {
     int table[2] [10];
+    
     storeTable(table, 0, 10, 2);
+    printf("\n");
+    storeTable(table, 0, 10, 3);
+
+    return 0;
 }
+
+void storeTable(int arr[] [10], int n, int m, int number){
+    for(int i = 0; i < m; i++) {
+        printf("%d \t", number * (i + 1));
+    }
+}
+
