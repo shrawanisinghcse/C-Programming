f858cba3834fcbb0d6b172fb430e4b7be37fbaba
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-08-26 13:17:51 +0530

    printing name using strings

---
+++ Strings/Basic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+    char name[] = {'S', 'H', 'R', 'A', 'W', 'A', 'N', 'I', '\0'};
+
+    char full_name[] = "Shrawani Singh";
+
+    printf("First Name : %s \n", name);
+    printf("Full Name : %s", full_name);
+
+    return 0;
+}
