36c7317b127e6628a2711d4d5f5fd2c345b859c2
Shrawani Singh <shrawanisingh.cse@gmail.com>
2026-08-29 12:57:25 +0530

    Reversing array

--- practice/RevArr.c
+++ practice/RevArr.c
@@ -1,10 +1,75 @@
 #include <stdio.h>
 
 int main() {
-    
-    int arr[] = {3, 4, 6, 5, 8};
 
-    for(int i = 0;i < 5; i++) {
-        printf("%d \t", arr[5-i-1]);
+    int n;
+    printf("How many elements are you going to enter in an array(<100) : ");
+    scanf("%d", &n);
+
+    int t;
+    printf("Choose your type : \n 1. Integers \n 2. Decimal values \n 3. Characters \n");
+    scanf("%d", &t);
+
+    printf("Enter %d elements of respective type : ", n);
+
+    if(t ==1) {
+        int arr[100];
+    for(int i = 0; i < n; i++) {
+        if(scanf("%d", &arr[i]) != 1){
+            printf("ERROR : You have entered something other than an integer.");
+            return 1;
+        }
     }
+    
+    for(int i = 0; i < n/2; i++) {
+        int FirstVal = arr[i];
+        int SecondVal = arr[n-i-1];
+        arr[i] = SecondVal;
+        arr[n-i-1] = FirstVal;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d \t", arr[i]);
+    }
+    }
+    else if(t == 2) {
+        float arr[100];
+        for(int i = 0; i < n; i++) {
+       if(scanf("%f", &arr[i]) != 1){
+            printf("ERROR : You have entered something other than a decimal value.");
+            return 1;
+        }
+    }
+    for(int i = 0; i < n/2; i++) {
+        float FirstVal = arr[i];
+        float SecondVal = arr[n-i-1];
+        arr[i] = SecondVal;
+        arr[n-i-1] = FirstVal;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%f \t", arr[i]);
+    }
+    }
+    else if(t == 3) {
+        char arr[100];
+        for(int i = 0; i < n; i++) {
+       if(scanf("%c", &arr[i]) != 1){
+            printf("ERROR : You have entered something other than a character.");
+            return 1;
+        }
+    }
+    for(int i = 0; i < n/2; i++) {
+        char FirstVal = arr[i];
+        char SecondVal = arr[n-i-1];
+        arr[i] = SecondVal;
+        arr[n-i-1] = FirstVal;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%c \t", arr[i]);
+    }
+    }
+    else {
+        printf("INVALID INPUT");
+    }
+
+    return 0;
 }
